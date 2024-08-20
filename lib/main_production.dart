b6f5887596fa99fb54411dcccb1362ba4ef763c4
Ahmed Sakr <medosakr506@gmail.com>
2024-08-20 07:30:29 -0700

    [add] - compile time configurations for flavors.

---
+++ lib/main_production.dart
@@ -0,0 +1,9 @@
+import 'package:advanced_flutter/core/routing/app_router.dart';
+import 'package:advanced_flutter/doc_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
+}
